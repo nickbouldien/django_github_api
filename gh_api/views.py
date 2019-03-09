94b2d96fc5dabc505de8edb7e6628d5199b3c7ac
nick bouldien <nick.bouldien@gmail.com>
2019-03-09 00:10:57 -0600

    get params for saving data or rendering json for the user route and add check to make sure the request method is GET

--- gh_api/views.py
+++ gh_api/views.py
@@ -1,24 +1,41 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseNotAllowed
 import requests
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    routes = {
+        "/api/": "'help' - shows available routes",
+        "/api/ping": "est availability",
+        "/api/user/<github_username>/": "get github data for a given user",
+        "/api/user<github_username>/repos": "get github repo data for a given user"
+    }
+    return JsonResponse(routes)
 
 def test(request):
     return HttpResponse("pong")
 
-
 # github api # https://developer.github.com/v3/
 
 def user_repos(request, username):
+    if request.method != 'GET':
+        return HttpResponseNotAllowed(['GET'])
+
     url = 'https://api.github.com/users/' + username + '/repos'
     r = requests.get(url)
     print(r.status_code)
 
     return JsonResponse(r.json(), safe=False)
 
-def user(request, username):
+def user(request, username = "nickbouldien"):
+    if request.method != 'GET':
+        return HttpResponseNotAllowed(['GET'])
+
+    save_param = request.GET.get('save', False)
+    json_param = request.GET.get('json', False)
+
+    save_data = True if save_param == "true" or save_param == "1" else False
+    render_json = True if json_param == "true" or json_param == "1" else False
+
     url = 'https://api.github.com/users/' + username
     r = requests.get(url)
     print(r.status_code)
