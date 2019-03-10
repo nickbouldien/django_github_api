1ec0e18d28e8077ad6f5537b3b6b4e6823b71714
nick bouldien <nick.bouldien@gmail.com>
2019-03-10 17:37:59 -0500

    can now return user data in the user.html file or as json with the json flag.  add migration for user model

--- gh_api/models.py
+++ gh_api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    avatar_url = models.CharField(max_length = 50)
+    blog = models.CharField(max_length = 50)
+    bio = models.TextField(max_length = 200)
+    company = models.CharField(max_length = 50)
+    location = models.CharField(max_length = 50)
+    login = models.CharField(max_length = 50)
+    followers = models.IntegerField(default = 0)
+    following = models.IntegerField(default = 0)
+    name = models.CharField(max_length = 50)
+    public_gists = models.IntegerField(default = 0)
+    public_repos = models.IntegerField(default = 0)
+    url = models.CharField(max_length = 50)
+    account_created_at = models.DateTimeField()
+    account_updated_at = models.DateTimeField()
+
+    def __repr__(self):
+        return "User('{}', '{}', '{}')".format(self.login, self.name, self.url)
+
+    def __str__(self):
+        return "'{}' - '{}'".format(self.login, self.name)
--- gh_api/views.py
+++ gh_api/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse, HttpResponseNotAllowed
+from .models import User
 import requests
+import json
 
 def index(request):
     routes = {
@@ -15,7 +17,6 @@ def test(request):
     return HttpResponse("pong")
 
 # github api # https://developer.github.com/v3/
-
 def user_repos(request, username = "nickbouldien"):
     if request.method != 'GET':
         return HttpResponseNotAllowed(['GET'])
@@ -34,8 +35,8 @@ def user(request, username = "nickbouldien"):
     save_param = request.GET.get('save', False)
     json_param = request.GET.get('json', False)
 
-    save_data = True if save_param == "true" or save_param == "1" else False
-    render_json = True if json_param == "true" or json_param == "1" else False
+    save_data = save_param == "true" or save_param == "1"
+    render_json = json_param == "true" or json_param == "1"
 
     url = 'https://api.github.com/users/' + username
 
@@ -43,4 +44,31 @@ def user(request, username = "nickbouldien"):
     r = requests.get(url, headers = headers)
     print(r.status_code)
 
-    return JsonResponse(r.json())
+    json_data = r.json()
+    user_data = {}
+
+    if r.status_code == requests.codes.ok:
+        user_data['avatar_url'] = json_data['avatar_url']
+        user_data['blog'] = json_data['blog']
+        user_data['bio'] = json_data['bio']
+        user_data['company'] = json_data['company']
+        user_data['location'] = json_data['location']
+        user_data['login'] = json_data['login']
+        user_data['followers'] = json_data['followers']
+        user_data['following'] = json_data['following']
+        user_data['name'] = json_data['name']
+        user_data['public_gists'] = json_data['public_gists']
+        user_data['public_repos'] = json_data['public_repos']
+        user_data['url'] = json_data['url']
+        user_data['account_created_at'] = json_data['created_at']
+        user_data['account_updated_at'] = json_data['updated_at']
+    else:
+        # return bad status
+        pass
+
+    user = {'user': user_data}
+
+    if render_json:
+        return JsonResponse(user)
+    
+    return render(request, 'gh_api/user.html', user)
